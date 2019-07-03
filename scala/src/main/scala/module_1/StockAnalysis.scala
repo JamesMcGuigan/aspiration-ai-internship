5246da076141de6304327a92573703843465a8cc
JamesMcGuigan <james.mcguigan.github@gmail.com>
2019-07-03 19:10:02 +0100

    module 1 | scala | StockAnalysis.filter_not_eq( List[StockPrice] )

--- scala/src/main/scala/module_1/StockAnalysis.scala
+++ scala/src/main/scala/module_1/StockAnalysis.scala
@@ -4,7 +4,15 @@ object StockAnalysis extends App {
   val input_csv_filename:  String = if (args.length >= 1) args(0) else "../data/stocks/Mid_Cap/MUTHOOTFIN.csv"
   val output_csv_filename: String = if (args.length >= 2) args(1) else "../data/output/week2-scala.csv"
 
-  val data: List[StockPrice] = StockPriceCSV.read(input_csv_filename)
+  var data: List[StockPrice] = StockPriceCSV.read(input_csv_filename)
+  data = filter_not_eq(data)
+
+
   StockPriceCSV.print(data)
   StockPriceCSV.write(output_csv_filename, data)
+
+  def filter_not_eq( stockPrices: List[StockPrice]): List[StockPrice] = {
+    stockPrices.filter( _.Series != "EQ" )
+  }
+
 }
