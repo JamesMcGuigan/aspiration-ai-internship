02b7bac25a5d83e74deeecbc4babc80ae99243df
JamesMcGuigan <james.mcguigan.github@gmail.com>
2019-07-06 15:55:08 +0100

    module 1 | scala | move CubeCalculator to seperate module

--- scala/src/main/scala/CubeCalculator/CubeCalculator.scala
+++ scala/src/main/scala/CubeCalculator/CubeCalculator.scala
@@ -1,4 +1,4 @@
-package module_1
+package CubeCalculator
 
 object CubeCalculator extends App {
 
--- scala/src/test/scala/CubeCalculator/CubeCalculatorTest.scala
+++ scala/src/test/scala/CubeCalculator/CubeCalculatorTest.scala
@@ -1,4 +1,4 @@
-package module_1
+package CubeCalculator
 
 class CubeCalculatorTest extends org.scalatest.FunSuite {
   test("CubeCalculator.cube") {
